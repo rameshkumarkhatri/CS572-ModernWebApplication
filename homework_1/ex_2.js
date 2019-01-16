46c1964d2a68031670f5f452bceac01554b93db4
Ramesh Kumar <r.khatri91@gmail.com>
2019-01-16 14:44:44 -0600

    days of week added array and objects too..

--- homework_1/ex_2.js
+++ homework_1/ex_2.js
@@ -1,14 +1,20 @@
 function isWeekend() {
     const todayDate = new Date();
     const day =     todayDate.getDay();
+    
     // const days = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend'];
     // return days[day];
 
-    switch(day) {
-        case 0:  return "weekend";
-        case 6: return "weekend";
-        default:  return "weekday";
-    }
+    // or
+    const days = {0:'weekend', 1: 'weekday', 2: 'weekday', 3: 'weekday', 4: 'weekday', 5: 'weekday', 6:'weekend'};
+    return days[day];
+    
+    //or
+    // switch(day) {
+    //     case 0:  return "weekend";
+    //     case 6: return "weekend";
+    //     default:  return "weekday";
+    // }
 }
 
 console.log(isWeekend());
