cf7397c0aa448d8cd04de5629ecee4b9d1042609
Ramesh Kumar <r.khatri91@gmail.com>
2019-01-30 14:17:03 -0600

    Homework lesson 12 completed. error resolved

---
+++ homework12/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+  <p>
+        Counter Component:
+        <app-counter (counterEmitter)="counterChange($event)">></app-counter>
+      </p>
+      <p>Component Counter Value:{{counter}}</p>
+  `
+})
+export class AppComponent {
+  title = 'homework12';
+
+  public counter:number = 1;
+
+
+  counterChange(counterValue) {
+    this.counter = counterValue;
+    console.log("counterChange method");
+  }
+
+
+}
---
+++ homework12/src/app/counter/counter.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  // templateUrl: './counter.component.html',
+  // styleUrls: ['./counter.component.css']
+  template: `
+    <button type="button" (click) = "decrement()">-</button>
+    {{counterValue}}
+    <button type="button" (click) = "increment()">+</button>
+  
+  `
+})
+export class CounterComponent implements OnInit {
+  // private counterValue: number = 0;
+  @Input('counter') counterValue: number =0;
+
+  increment() {
+    this.counterValue++;
+    this.emitCounterValue();
+  }
+
+  decrement() {
+    if (this.counterValue > 0)
+    {
+      this.counterValue--; 
+      this.emitCounterValue();
+    } 
+  }
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+    console.log("nginit counter "+" counterValue "+this.counterValue);
+    // this.counterValue = this.counter;
+  }
+
+
+  @Output() counterEmitter = new EventEmitter();
+
+  emitCounterValue() {
+    console.log("emitCounterValue method");
+    this.counterEmitter.emit(this.counterValue);
+  }
+
+}
